069ec29e9accab9a5641cdf1cf1762da7ba3b438
madelynvalazquez304-cloud <madelynvalazquez304@gmail.com>
2026-07-27 02:00:32 +0300

    jh

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
+function rewriteAdminRoute() {
+  return {
+    name: 'rewrite-bigadmin-route',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === '/bigadmin' || req.url === '/bigadmin/') {
+          req.url = '/bigadmin.html';
+        }
+        next();
+      });
+    },
+    configurePreviewServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === '/bigadmin' || req.url === '/bigadmin/') {
+          req.url = '/bigadmin.html';
+        }
+        next();
+      });
+    }
+  };
+}
+
 export default defineConfig({
+  plugins: [rewriteAdminRoute()],
   build: {
     rollupOptions: {
       input: {
